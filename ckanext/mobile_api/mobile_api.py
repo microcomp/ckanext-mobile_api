6b771b4bcd22b9a5d9ffa7402237fc4980bf4061
János Farkas <farkas48@uniba.sk>
2016-03-29 09:08:55 +0200

    Update mobile_api.py

--- ckanext/mobile_api/mobile_api.py
+++ ckanext/mobile_api/mobile_api.py
@@ -57,168 +57,167 @@ _text = sqlalchemy.text
 def dataset_list(context, data_dict=None):
     '''
     '''
-    schema = (context.get('schema') or
-              logic.schema.default_package_search_schema())
-    data_dict, errors = _validate(data_dict, schema, context)
-    data_dict.update(data_dict.get('__extras', {}))
-    data_dict.pop('__extras', None)
-    if errors:
-        raise ValidationError(errors)
-    model = context['model']
-    session = context['session']
-    _check_access('package_search', context, data_dict)
-    data_dict['extras'] = data_dict.get('extras', {})
-    for key in [key for key in data_dict.keys() if key.startswith('ext_')]:
-        data_dict['extras'][key] = data_dict.pop(key)
-    for item in plugins.PluginImplementations(plugins.IPackageController):
-        data_dict = item.before_search(data_dict)
-    abort = data_dict.get('abort_search', False)
-
-    if data_dict.get('sort') in (None, 'rank'):
-        data_dict['sort'] = 'metadata_modified desc'
-
-    results = []
-    if not abort:
-        data_source = 'data_dict' if data_dict.get('use_default_schema',
-            False) else 'validated_data_dict'
-        data_dict['fl'] = 'id {0}'.format(data_source)
-        fq = data_dict.get('fq', '')
-        if not context.get('ignore_capacity_check', False):
-            fq = ' '.join(p for p in fq.split(' ')
-                            if not 'capacity:' in p)
-            data_dict['fq'] = fq + ' capacity:"public"'
-        extras = data_dict.pop('extras', None)
-
-        query = search.query_for(model.Package)
-        query.run(data_dict)
-        data_dict['extras'] = extras
-
-        for package in query.results:
-            package, package_dict = package['id'], package.get(data_source)
-            pkg_query = session.query(model.PackageRevision)\
-                .filter(model.PackageRevision.id == package)\
-                .filter(_and_(
-                    model.PackageRevision.state == u'active',
-                    model.PackageRevision.current == True
-                ))
-            pkg = pkg_query.first()
-            if not pkg:
-                log.warning('package %s in index but not in database' % package)
-                continue
-            if package_dict:
-                package_dict = json.loads(package_dict)
-                if context.get('for_view'):
-                    for item in plugins.PluginImplementations( plugins.IPackageController):
-                        package_dict = item.before_view(package_dict)
-                results.append(package_dict)
-            else:
-                results.append(model_dictize.package_dictize(pkg,context))
-
-        count = query.count
-        facets = query.facets
-    else:
-        count = 0
-        facets = {}
-        results = []
-
-    search_results = {
-        'count': count,
-        'facets': facets,
-        'results': results,
-        'sort': data_dict['sort']
-    }
-    restructured_facets = {}
-    for key, value in facets.items():
-        restructured_facets[key] = {
-                'title': key,
-                'items': []
-                }
-        for key_, value_ in value.items():
-            new_facet_dict = {}
-            new_facet_dict['name'] = key_
-            if key in ('groups', 'organization'):
-                group = model.Group.get(key_)
-                if group:
-                    new_facet_dict['display_name'] = group.display_name
-                else:
-                    new_facet_dict['display_name'] = key_
-            elif key == 'license_id':
-                license = model.Package.get_license_register().get(key_)
-                if license:
-                    new_facet_dict['display_name'] = license.title
-                else:
-                    new_facet_dict['display_name'] = key_
-            else:
-                new_facet_dict['display_name'] = key_
-            new_facet_dict['count'] = value_
-            restructured_facets[key]['items'].append(new_facet_dict)
-    search_results['search_facets'] = restructured_facets
-    for item in plugins.PluginImplementations(plugins.IPackageController):
-        search_results = item.after_search(search_results,data_dict)
-    for facet in search_results['search_facets']:
-        search_results['search_facets'][facet]['items'] = sorted(
-                search_results['search_facets'][facet]['items'],
-                key=lambda facet: facet['display_name'], reverse=True)
-    result = []
-    facets__ = {}
-    o_list = logic.get_action('organization_list')(context,{})
-    #full_org_data = [ logic.get_action("organization_show")(context, {"id":x}) for x in o_list]
-    full_org_data = [ model.Session.query(model.Group).filter(model.Group.name == x).first()  for x in o_list]
-    #facets__['organization']= [{"display_name":x["display_name"], "id":x["id"]} for x in full_org_data ]
-    facets__['organization']= [{"display_name":x.display_name, "id":x.id} for x in full_org_data ]
-
-    facets__['tags'] = logic.get_action('tag_list')(context,{})
-    formats = set()
-    license_type = []
-    all_license_types = logic.get_action("license_list")(context, {})
-    for license in all_license_types:
-        license_type.append({"title":license["title"], "id":license["id"]})
-    for i in 'abcdefghijklmnopqrstuvwxyz':
-        for j in logic.get_action('format_autocomplete')(context, {'q':i}):
-            formats.add(j)
-    facets__['res_format'] = [x for x in formats]
-    facets__['license_type'] = [x for x in license_type]      
-    facet_titles = [('organization', u'Organiz\xe1cie'), ('tags', u'Tagy'), ('res_format', u'Form\xe1ty'), ('license_id', u'Licencia')]
-    context = {'model': model, 'session': model.Session,
-                       'user': c.user or c.author, 'auth_user_obj': c.userobj}
-    data_dict = {
-        'q': '*:*',
-        'facet.field': g.facets,
-        'rows': 4,
-        'start': 0,
-        'sort': 'views_recent desc',
-        'fq': 'capacity:"public"'
-    }
-    query = logic.get_action('package_search')(
-        context, data_dict)
-
-    search_results['facets'] =  query['search_facets'] #facets__
-    search_results['facets'].pop('groups')
-    for dataset in search_results['results']:
-        helper = {}
-        helper['display_name'] = dataset['title']
-        helper['id'] = dataset['id']
-        helper['organization'] = {'name':dataset['organization']['title'], 'id':dataset['organization']['id']}
-        #helper['resources'] = [{'name':x['name'], 'description':x['description'], 'format':x['format'], 'id':x['id'], 'url':x['url']} for x in dataset['resources']]
-        helper['resource_type'] = [x for x in set([ x['format'] for x in dataset['resources']])]
-        helper['tags'] = [x["display_name"] for x in dataset['tags']]
-        helper["metadata_created"] = dataset["metadata_created"]
+    data_dict["facet.field"] = ["organization", "tags", "res_format", "license_id"]
+    result = logic.get_action("package_search")(context, data_dict)
+    for i in range(len(result["results"])):
         try:
-            helper["publish_date"] = dataset["publish_date"]
+        	result["results"][i].pop("author_email")
         except KeyError:
-            helper["publish_date"] = None
-        helper["metadata_modified"] = dataset["metadata_modified"]
-        helper["num_resources"] = len(dataset["resources"])
-        helper["license_title"] = dataset["license_title"]
-        author = model.Session.query(model.User).filter(model.User.id == dataset["author"]).first()
-        if author == None:
-            helper["author"] =dataset["author"]
-        else:
-            helper["author"] = author.display_name
+        	pass
+        try:
+        	result["results"][i].pop("maintainer")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("relationships_as_object")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("private")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("maintainer_email")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("state")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("version")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("spatial")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("creator_user_id")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("type")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("tracking_summary")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("groups")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("relationships_as_subject")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("isopen")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("url")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i]["organization"].pop("description")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i]["organization"].pop("created")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i]["organization"]["name"] = result["results"][i]["organization"].pop("title")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i]["organization"].pop("revision_timestamp")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i]["organization"].pop("is_organization")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i]["organization"].pop("state")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i]["organization"].pop("image_url")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i]["organization"].pop("revision_id")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i]["organization"].pop("type")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i]["organization"].pop("approval_status")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("name")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("notes")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("owner_org")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("license_url")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("revision_timestamp")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("license_id")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i].pop("revision_id")
+        except KeyError:
+        	pass
+        try:
+        	result["results"][i]["num_tags"] = len(result["results"][i]["tags"])
+        except KeyError:
+        	pass
+        tags = []
+        for j in range(len(result["results"][i]["tags"])):
+        	tags.append(result["results"][i]["tags"][j].pop("display_name"))
 
-        result.append(helper)
-    search_results['results'] = result
-    return search_results
+        res_type = set()
+        for j in range(len(result["results"][i]["resources"])):
+        	res_type.add(result["results"][i]["resources"][j].pop("format"))
+
+        try:
+            result["results"][i]["publish_date"] = result["results"][i]["publish_date"]
+        except KeyError:
+            result["results"][i]["publish_date"] = None
+        result["results"][i].pop("resources")
+        result["results"][i]["resource_type"] = [x for x in res_type]
+
+        result["results"][i]["tags"] = tags
+
+
+        result["results"][i]["display_name"] = result["results"][i].pop("title")
+
+        author = model.Session.query(model.User).filter(model.User.id == result["results"][i]["author"]).first()
+        if author != None:
+            result["results"][i]["author"] = author.display_name
+    result["facets"] = result["search_facets"]
+    result.pop("search_facets")
+    return result
 
 @toolkit.side_effect_free
 def package_show(context, data_dict=None):
